5cb79634ee936a7967051aeb7f397b5115b4642e
서건희 <seogunhee4@gmail.com>
2021-10-07 21:42:02 +0900

    [FIX]auth

--- src/main/java/dsm/project/findapple/service/user/UserService.java
+++ src/main/java/dsm/project/findapple/service/user/UserService.java
@@ -1,7 +1,4 @@
 package dsm.project.findapple.service.user;
 
-import dsm.project.findapple.payload.request.SignUpRequest;
-
 public interface UserService {
-    void signUp(SignUpRequest signUpRequest);
 }
