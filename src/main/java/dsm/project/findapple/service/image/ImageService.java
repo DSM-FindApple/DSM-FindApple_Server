d7ce41033fb2407a2fc64741c5ca7153f1ee537c
서건희 <seogunhee4@gmail.com>
2021-10-21 09:57:38 +0900

    [FIX]image

--- src/main/java/dsm/project/findapple/service/image/ImageService.java
+++ src/main/java/dsm/project/findapple/service/image/ImageService.java
@@ -1,1 +1,9 @@
+package dsm.project.findapple.service.image;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface ImageService {
+    void uploadImage(MultipartFile image, String imageName);
+    void delete(String imageName);
+    byte[] readImage(String imageName);
+}
