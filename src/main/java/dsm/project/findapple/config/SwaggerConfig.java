25d37a9b5c2b399b6357954dc500680f0074f24e
서건희 <seogunhee4@gmail.com>
2021-10-22 13:21:47 +0900

    [FIX]swagger

--- src/main/java/dsm/project/findapple/config/SwaggerConfig.java
+++ src/main/java/dsm/project/findapple/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package dsm.project.findapple.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +13,14 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Configuration
+@EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
