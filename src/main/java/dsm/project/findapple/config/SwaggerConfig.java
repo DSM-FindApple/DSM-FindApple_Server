3ebfcd977f87973af1b7fa55a84698c150c63c1f
서건희 <seogunhee4@gmail.com>
2021-10-22 11:50:03 +0900

    [ADD]swagger

---
+++ src/main/java/dsm/project/findapple/config/SwaggerConfig.java
@@ -0,0 +1,117 @@
+package dsm.project.findapple.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket apiAuth() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("apis")
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.POST, getAuthResponses())
+                .globalResponseMessage(RequestMethod.DELETE, getAuthResponses())
+                .globalResponseMessage(RequestMethod.PUT, getAuthResponses())
+                .globalResponseMessage(RequestMethod.GET, getAuthResponses())
+                .apiInfo(this.getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("dsm.project.findapple.controller"))
+                .build();
+    }
+
+    //error option
+    public List<ResponseMessage> getAuthResponses() {
+        List<ResponseMessage> builders = new ArrayList<>();
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(500)
+                        .message("server error")
+                        .responseModel(new ModelRef("Error"))
+                        .build()
+        );
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(403)
+                        .message("Forbidden(no token)")
+                        .build()
+        );
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(401)
+                        .message("Unauthorized")
+                        .build()
+        );
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(400)
+                        .message("Bad request")
+                        .build()
+        );
+        return builders;
+    }
+
+    //error option
+    public List<ResponseMessage> getUserResponses() {
+        List<ResponseMessage> builders = new ArrayList<>();
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(400)
+                        .message("Bad Ex Request")
+                        .build()
+        );
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(400)
+                        .message("Bad request user image")
+                        .build()
+        );
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(409)
+                        .message("user already signed")
+                        .build()
+        );
+        builders.add(
+                new ResponseMessageBuilder()
+                        .code(500)
+                        .message("server error")
+                        .responseModel(new ModelRef("Error"))
+                        .build()
+        );
+
+        return builders;
+    }
+
+    //api info
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "FindApple API Docs",
+                "FindApple Project Docs",
+                "1.0.0",
+                "Terms of Service URL",
+                "Contact Name",
+                "License",
+                "License URL"
+        );
+    }
+
+    //resource handlers
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+    }
+}
