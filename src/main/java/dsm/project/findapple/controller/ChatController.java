0133b8ff37966114c87d0213af7a99d502408b89
서건희 <seogunhee4@gmail.com>
2021-11-22 09:27:42 +0900

    [FIX]cors

--- src/main/java/dsm/project/findapple/controller/ChatController.java
+++ src/main/java/dsm/project/findapple/controller/ChatController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatController {
